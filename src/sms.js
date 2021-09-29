805757db3ed657e48d28a96646c0990c1cf9a5f7
Dai Huynh <dai.huynh@ucdenver.edu>
2021-09-29 10:13:06 -0600

    Added phone number and access code field

---
+++ src/sms.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class sms extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor='to'>To:</label>
+          <input type='tel' name='to' id='to' />
+        </div>
+        <div>
+          <label htmlFor='body'>Body:</label>
+          <textarea name='body' id='body' />
+        </div>
+        <button type='submit'>Send message</button>
+      </form>
+    );
+  }
+}
+
+export default sms;
